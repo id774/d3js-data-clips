254078f722007b20b07be98ec44ec0c93a634717
id774 <idnanashi@gmail.com>
2014-11-24 16:10:32 +0900

    8 Change ERB to Haml

--- web.rb
+++ web.rb
@@ -1,26 +1,26 @@
 require 'sinatra'
 
 get '/' do
-  erb :index
+  haml :index
 end
 
 get '/ayakashino-tuzumi' do
-  erb :ayakashino_tuzumi
+  haml :ayakashino_tuzumi
 end
 
 get '/binzume-jigoku' do
-  erb :binzume_jigoku
+  haml :binzume_jigoku
 end
 
 get '/kichigai-jigoku' do
-  erb :kichigai_jigoku
+  haml :kichigai_jigoku
 end
 
 get '/shigono-koi' do
-  erb :shigono_koi
+  haml :shigono_koi
 end
 
 get '/shojo-jigoku' do
-  erb :shojo_jigoku
+  haml :shojo_jigoku
 end
 
